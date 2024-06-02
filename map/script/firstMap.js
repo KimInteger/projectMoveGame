41d9064c4e2f7aa72442d5fb2593c47c6fd5d730
KimInteger <kjs20151240@gmail.com>
2024-06-02 10:55:27 +0900

    이벤트 수정 및 할당, 왕관이 나오게함 setInterval사용

--- map/script/firstMap.js
+++ map/script/firstMap.js
@@ -4,6 +4,10 @@ const third = document.getElementById('third');
 const fourth = document.getElementById('fourth');
 const fifth = document.getElementById('fifth');
 const sixth = document.getElementById('sixth');
+sixth.style.overflow = 'visible';
+
+
+const crown = sixth.children[0];
 
 let player = document.createElement('div');
 player.style.width = '50px';
@@ -13,6 +17,7 @@ player.style.backgroundColor = 'black'
 
 first.appendChild(player);
 
+let percent = 200;
 let nextStage
 
 function openWindow(){
@@ -33,8 +38,19 @@ document.addEventListener('keydown', (e)=>{
       second.appendChild(player);  
     } else if (fifth.children.length ===1) {
       fifth.textContent = '';
-      sixth.appendChild(player);
-      openWindow();
+      crown.style.visibility = 'visible';
+      // setinterval을 걸고. 사이즈를 올리고 줄였다가 원래크기로 돌아오면, 
+      let sizeUp = setInterval(()=>{
+      percent++;
+      crown.style.width = percent + 'px';
+      crown.style.height = percent + 'px';
+      if (percent > 350) {
+        clearInterval(sizeUp)
+        setTimeout(()=>{
+          openWindow();
+        },2000);
+      }
+    },16);
     } else {
       alert('움직일 수 없는 타일입니다!');
     }
