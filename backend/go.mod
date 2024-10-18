3821fa2c68fc4cb5ccff23554dbae78ce2375ddb
김정수 <kjs20151240@gmail.com>
2024-10-18 19:16:39 +0900

    init | webpack.config.js
    번들링 html파일 하나로

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module simpleserve
+
+go 1.23.1
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,89 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+class CustomPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('CustomPlugin', (compilation) => {
+      HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
+        'CustomPlugin',
+        (data, cb) => {
+          data.headTags = data.headTags.filter(
+            (tag) => tag.tagName !== 'script',
+          );
+          data.bodyTags = data.bodyTags.filter(
+            (tag) => tag.tagName !== 'script',
+          );
+
+          const scriptTag = {
+            tagName: 'script',
+            voidTag: false,
+            attributes: {
+              type: 'text/javascript',
+            },
+            innerHTML: compilation.assets['bundle.js'].source(),
+          };
+
+          data.bodyTags.push(scriptTag);
+
+          delete compilation.assets['bundle.js'];
+
+          cb(null, data);
+        },
+      );
+    });
+  }
+}
+
+const entryPoint = './src/index.tsx';
 
 module.exports = {
-  mode: 'development', // 모드 설정
-  entry: './src/index.tsx', // 진입점
+  entry: entryPoint,
   output: {
-    path: path.resolve(__dirname, 'dist'), // 출력 경로
-    filename: 'bundle.js', // 출력 파일 이름
-    publicPath: './', // 상대 경로로 변경
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: ['.ts', '.js', '.tsx', '.jsx', '.css'],
   },
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
-        use: 'babel-loader', // babel-loader 사용
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // 순서 중요 css가 읽히고 style이 읽혀야 한다. script화 이후 DOM으로 넣어주기
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/, // 자스인지 확인할 거야.
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx$/, // 타스인지 확인할 거야.
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'), // public 폴더의 index.html 파일 경로
-      filename: 'index.html', // 출력할 HTML 파일 이름
+      template: './public/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
     }),
+    new CustomPlugin(),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 };
