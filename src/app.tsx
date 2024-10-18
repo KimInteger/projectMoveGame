6a6cfa4f693ca430c02380f275ac20e914c36e89
김정수 <kjs20151240@gmail.com>
2024-10-18 17:48:31 +0900

    feat | app.tsx
    route 추가

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EntryPage from './pages/entryPage';
+import TutorialZeroOne from './pages/tutorialMapZeroOne';
 
 const App = () => {
   return (
-    <>
-      <h1>Hello Refactoring!</h1>
-      <h1>Now Start</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<EntryPage />} />
+        <Route path="/tutoZeroOne" element={<TutorialZeroOne />} />
+        <Route path="/tutoZeroTwo" />
+        <Route path="/tutoZeroThree" />
+      </Routes>
+    </Router>
   );
 };
 
