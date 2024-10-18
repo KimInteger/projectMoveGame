cb755b50476d95e7b7b1b5baeeb45db6cd0ea8f5
김정수 <kjs20151240@gmail.com>
2024-10-18 19:24:03 +0900

    fix | pages
    불필요한 호출 삭제

--- src/pages/entryPage.tsx
+++ src/pages/entryPage.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import nextStage from '../lib/mapFunc/mapFuncNextStage';
 
 const EntryPage = () => {
-  const nextRoute = nextStage('/tutoZeroOne');
   return (
     <>
       <div>
         <h1>Game Entry Page</h1>
         <div>
-          <button onClick={nextRoute}>Start</button>
+          <button onClick={nextStage('/tutoZeroOne')}>Start</button>
         </div>
       </div>
     </>
--- src/pages/tutorialMapZeroTwo.tsx
+++ src/pages/tutorialMapZeroTwo.tsx
@@ -3,10 +3,9 @@ import TutorialMap from '../ui/basicMapTemplate/tutorialTemplate';
 import nextStage from '../lib/mapFunc/mapFuncNextStage';
 
 const TutorialZeroTwo = () => {
-  const nextRoute = nextStage('/tutoZeroThree');
   return (
     <>
-      <TutorialMap title="튜토리얼 2" routeFunc={nextRoute}>
+      <TutorialMap title="튜토리얼 2" routeFunc={nextStage('/tutoZeroThree')}>
         <p>
           안녕 여긴 튜토리얼 2이란다. 조선의 궁궐에 당도한 것을 환영하오
           나썬이여
