a66166667d521e1f6fb4aee3098d4db07d8c1818
김정수 <kjs20151240@gmail.com>
2024-10-21 22:11:13 +0900

    feat | entryPage.tsx
    toggle로 테마 context조절

--- src/pages/entryPage.tsx
+++ src/pages/entryPage.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useTheme } from '../state/themeContext'; // useTheme 불러오기
 import nextStage from '../lib/mapFunc/mapFuncNextStage';
 
 const EntryPage = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <>
-      <div>
+      <div className={theme}>
         <h1>Game Entry Page</h1>
-        <div>
-          <button onClick={nextStage('/tutoZeroOne')}>Start</button>
-        </div>
+        <button onClick={nextStage('/tutoZeroOne')}>Start</button>
+        <button onClick={toggleTheme}>
+          Toggle to {theme === 'light' ? 'dark' : 'light'} Theme
+        </button>
       </div>
     </>
   );
--- src/ui/basicMapComponent/basicMapBlock.tsx
+++ src/ui/basicMapComponent/basicMapBlock.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../../style/basicMap.css';
 import { BasicMapBlockProps } from '../../interface/interfaceMap/basicMapBlockProps';
+import { useTheme } from '../../state/themeContext'; // useTheme import
 
 const BasicMapBlock: React.FC<BasicMapBlockProps> = ({ area, content }) => {
+  const { theme } = useTheme(); // 현재 테마 가져오기
+
   return (
-    <div className="basicMapBlock" style={{ gridArea: area }}>
+    <div
+      className={`basicMapBlock ${theme}`} // 테마에 따라 클래스 추가
+      style={{ gridArea: area }}
+    >
       {content}
     </div>
   );
