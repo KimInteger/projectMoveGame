ca04c157b533803accaaadc8a549c2af862b4348
김정수 <kjs20151240@gmail.com>
2024-10-21 21:44:43 +0900

    test | tutorialMapZeroOne.tsx
    ZeroOne에서 테스트 성공

--- src/pages/tutorialMapZeroOne.tsx
+++ src/pages/tutorialMapZeroOne.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import TutorialMap from '../ui/basicMapTemplate/tutorialTemplate';
 import nextStage from '../lib/mapFunc/mapFuncNextStage';
+import BasicMapTemplate from '../ui/basicMapTemplate/basicMap';
+import { gridAreas } from '../static/basicMapGridArea/staticBasicMapAreas';
 
 const TutorialZeroOne = () => {
+  const area = gridAreas.first;
   return (
     <>
       <TutorialMap title="튜토리얼 1" routeFunc={nextStage('/tutoZeroTwo')}>
@@ -11,6 +14,7 @@ const TutorialZeroOne = () => {
           나썬이여
         </p>
       </TutorialMap>
+      <BasicMapTemplate areas={area} />
     </>
   );
 };
