1f45ec4f4d0178c278379447075ff2af7095ad1d
김정수 <kjs20151240@gmail.com>
2024-10-21 22:09:57 +0900

    feat | ThemeContext
    useContext를 사용하여 theme를 조절

---
+++ src/state/themeContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useContext } from 'react';
+import { ThemeContextProps } from '../interface/context/themeContextInterface';
+import { Theme } from '../interface/context/themeContextInterface';
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
