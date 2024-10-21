3d2ca8cb285f6a8b8763c9470d42892a3796aea7
김정수 <kjs20151240@gmail.com>
2024-10-21 21:41:45 +0900

    style | index.tsx
    global.css할당

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React, { DOMElement } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import './style/global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
